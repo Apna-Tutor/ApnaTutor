5b3077eeefbd77150947ea3fe542e0bb264a0a79
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2022-09-01 20:25:18 +0530

    Initial commit

---
+++ app/src/main/java/com/debuggers/apna_tutor/fragments/HomeFragment.java
@@ -0,0 +1,34 @@
+package com.debuggers.apna_tutor.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.debuggers.apna_tutor.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
