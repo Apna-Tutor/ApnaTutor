6a89b4ede3be4b3b62683cfe1b2ed2600c7da5dd
Pritam Dey <pritamdey2001@gmail.com>
2022-09-10 18:17:44 +0530

    Add ActivitySettings, ActivityChangePassword, ActivityUpdateProfile

---
+++ app/src/main/java/com/debuggers/apnatutor/Activities/ActivitySettings.java
@@ -0,0 +1,16 @@
+package com.debuggers.apnatutor.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.debuggers.apnatutor.R;
+
+public class ActivitySettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
---
+++ app/src/main/java/com/debuggers/apnatutor/Activities/ActivityUpdateProfile.java
@@ -0,0 +1,16 @@
+package com.debuggers.apnatutor.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.debuggers.apnatutor.R;
+
+public class ActivityUpdateProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_profile);
+    }
+}
